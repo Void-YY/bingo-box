9b76f751bf5aa8a149166067d2e8a80bd0a38443
voidWh <ysoseriousc@gmail.com>
2021-08-29 17:14:22 +0900

    feat: bingo demo

--- components/Balls/Balls.tsx
+++ components/Balls/Balls.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: VoidY
  * @Date: 2021-08-28 22:58:00
- * @LastEditTime: 2021-08-28 23:07:13
+ * @LastEditTime: 2021-08-29 17:01:10
  * @LastEditors: VoidY
  * @Description:
  */
@@ -9,21 +9,26 @@ import range from "lodash-es/range";
 import styles from "./Balls.module.scss";
 interface Props {
   value: number;
+  className?: string;
 }
-
-interface NumbersProps {
+interface AreaProps {
   numbers: number;
+  activeNumber?: number;
 }
 
 const ListItem: React.FC<Props> = (props: Props) => {
-  return <div className={styles.ball}>{props.value}</div>;
+  return <div className={styles.ball +' '+ props.className}>{props.value}</div>;
 };
-const Balls = (props: NumbersProps): React.ReactElement => {
+const Balls = (props: AreaProps): React.ReactElement => {
   const numberMap = range(1, props.numbers + 1);
   return (
     <div className={styles.area}>
       {numberMap.map((number: number) => (
-        <ListItem key={number.toString()} value={number} />
+        <ListItem
+          className={props.activeNumber === number ? styles.active : ""}
+          key={number.toString()}
+          value={number}
+        />
       ))}
     </div>
   );
--- pages/bingo/index.tsx
+++ pages/bingo/index.tsx
@@ -1,19 +1,52 @@
 /*
  * @Author: VoidY
  * @Date: 2021-08-28 22:49:03
- * @LastEditTime: 2021-08-28 23:20:12
+ * @LastEditTime: 2021-08-29 17:10:08
  * @LastEditors: VoidY
  * @Description:
  */
 import type { NextPage } from "next";
 import Balls from "../../components/Balls/Balls";
-import styles from './index.module.scss'
+import random from "lodash-es/random";
+
+import shuffle from "lodash-es/shuffle";
+import { useEffect, useState } from "react";
+import range from "lodash-es/range";
+import styles from "./index.module.scss";
 const Bingo: NextPage = () => {
   const count = 75;
+  const [activeNumber, setActiveNumber] = useState(-1);
+  const [selectedCount, selectSelectedCount] = useState(0);
+  const [startedFlag, setStartedFlag] = useState(false);
+  const randomArray = shuffle(range(1, count + 1));
+
+  const randomRoll = () => {
+    setActiveNumber(random(selectedCount, randomArray.length - 1));
+  };
+  useEffect(() => {
+    if (startedFlag) {
+      setTimeout(() => randomRoll(), 1000);
+    }
+  }, [activeNumber]);
+
+  const start = () => {
+    setStartedFlag(true);
+    randomRoll();
+  };
+  const stop = () => {
+    setStartedFlag(false);
+    setActiveNumber(selectedCount);
+    selectSelectedCount(selectedCount + 1);
+  };
   return (
-    <div className={styles.main}>
-      <Balls numbers={count}></Balls>
-    </div>
+    <>
+      <div className={styles.main}>
+        <button className={styles.button} onClick={startedFlag ? stop : start}>
+          {startedFlag ? "stop" : "start"}
+        </button>
+        <Balls numbers={count} activeNumber={activeNumber}></Balls>
+      </div>
+    </>
   );
 };
 export default Bingo;
